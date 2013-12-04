818566717570a9871ba59b4524b82d1a798123a9
Trevor R. <>
2013-12-04 15:16:10 -0700

    Shorter Mechanum drive.

--- src/net/pvschools/robotics/javabot/practice/commands/MecanumDrive.java
+++ src/net/pvschools/robotics/javabot/practice/commands/MecanumDrive.java
@@ -24,24 +24,15 @@ public class MecanumDrive extends RoboCommand {
 
     protected void execute() 
     {
-        double calX = oi.getDriveX();
-        double calY = oi.getDriveY();
-        double calTwist = oi.getDriveTwist();
+        double calX     = oi.getDriveY(); 
+        double calY     = oi.getDriveX(); 
+        double calTwist = oi.getDriveTwist(); 
 
-        calX = Math.abs(calX) < .15
-            ? 0
-            : (1.04167 * calX * Math.abs(calX)) - .0416667;
+        calX     = Math.abs(calX)     < 0.15 ? 0 : calX; 
+        calY     = Math.abs(calY)     < 0.15 ? 0 : calY; 
+        calTwist = Math.abs(calTwist) < 0.15 ? 0 : calTwist;         
 
-        calY = Math.abs(calY) < .2
-            ? 0
-            : (1.04167 * calY * Math.abs(calY)) - .0416667;
-
-        double twistfactor = (oi.getDriveThrottle() + 1) / 2 + 1;
-        calTwist = Math.abs(calTwist) < .2
-            ? 0
-            : -((1.04167 * calTwist * Math.abs(calTwist)) - .0416667) / twistfactor;
-        
-        driveTrain.driveCartesian(calX, calY, calTwist, 0.0);
+        driveTrain.driveCartesian(calX, calY, calTwist, 0.0); 
         complete = false;
     }
 
