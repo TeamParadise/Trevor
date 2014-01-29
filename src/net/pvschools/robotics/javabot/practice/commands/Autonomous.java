cbbd18c41f59fdae3ef0c5e87206cb92faa4253e
student <student@10.90.33.229>
2014-01-29 15:25:33 -0700

    Fixes and implementing CommandGroup

---
+++ src/net/pvschools/robotics/javabot/practice/commands/Autonomous.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.pvschools.robotics.javabot.practice.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ * @author Trevor
+ */
+public class Autonomous extends CommandGroup {
+    
+    public Autonomous(){
+        
+    }
+    
+}
