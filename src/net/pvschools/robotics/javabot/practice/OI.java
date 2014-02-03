620487b5a442ce71320eeca1ad23674da284de98
student <student@10.90.33.229>
2014-02-03 15:08:44 -0700

    General Fixes

--- src/net/pvschools/robotics/javabot/practice/OI.java
+++ src/net/pvschools/robotics/javabot/practice/OI.java
@@ -1,23 +1,15 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Operator Interface methods
  */
 package net.pvschools.robotics.javabot.practice;
 
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import net.pvschools.robotics.javabot.practice.Systems.Piston;
-
 
 public class OI {
     private static OI instance;
 
     private final Joystick mainJoystick = new Joystick(Map.joystickPort);
-    private final Compressor compressor = new Compressor(Map.compressorSwitch, Map.compressorRelay);
-    private final Piston[] pistons = {new Piston(Map.shooterRelay, Map.shooterSolonoid)};
-    private final Gyro gyro = new Gyro(Map.gyroPort);
     
     public OI(){
         SmartDashboard.putNumber("damping", .5);
@@ -44,22 +36,6 @@ public class OI {
     
     public double getDriveTwist(){
         return mainJoystick.getTwist();
-    }
-    
-    public Gyro getGyro(){
-        return gyro;
-    }
-    
-    public Piston getPiston(int index){
-        return pistons[index];
-    }
-    
-    public Piston[] getPistons(){
-        return pistons;
-    }
-    
-    public Compressor getCompressor(){
-        return compressor;
     }
     
     public static OI getInstance(){
