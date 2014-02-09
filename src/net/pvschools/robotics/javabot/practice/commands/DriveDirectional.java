b0169d0deebf83bbd08bd157a7ad5e5d1c61ac94
jkoehring <jkoehring@jkoehring-e6510.exponent.com>
2014-02-09 12:07:52 -0700

    Add support for DriveDirectional getting parameters from the smart dashboard.

--- src/net/pvschools/robotics/javabot/practice/commands/DriveDirectional.java
+++ src/net/pvschools/robotics/javabot/practice/commands/DriveDirectional.java
@@ -5,6 +5,8 @@
  */
 package net.pvschools.robotics.javabot.practice.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 /**
  * Class for driving the robot at a fixed speed and direction.
  *
@@ -14,6 +16,11 @@ public class DriveDirectional extends RoboCommand
 {
     private double magnitude;
     private double direction;
+	
+	// The following are smart dashboard keys:
+	private String magnitudeKey = null;
+	private String directionKey = null;
+	private String timeoutKey = null;
     
     // Specifies speed and direction to drive the robot.
     public DriveDirectional(double magnitude, double direction)
@@ -23,9 +30,25 @@ public class DriveDirectional extends RoboCommand
         this.direction = direction;
     }
 
+    // Uses values from the smart dashboard to drive the robot.
+    public DriveDirectional(String magnitudeKey, String directionKey, String timeoutKey)
+    {
+        requires(driveTrain);
+        this.magnitudeKey = magnitudeKey;
+        this.directionKey = directionKey;
+		this.timeoutKey = timeoutKey;
+    }
+
     // Called just before this Command runs the first time
     protected void initialize()
     {
+		// See if we are using values from the smart dashboard:
+		if (null != magnitudeKey)
+		{
+			magnitude = SmartDashboard.getNumber(magnitudeKey);
+			direction = SmartDashboard.getNumber(directionKey);
+			setTimeout(SmartDashboard.getNumber(timeoutKey));
+		}
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -34,13 +57,10 @@ public class DriveDirectional extends RoboCommand
         driveTrain.drivePolar(magnitude, direction, 0);
     }
 
-    // Make this return true when this Command no longer needs to run execute()
-    //
-    // Note that this command is typically called from the Autonomous
-    //
+    // Note that when there is no timeout value set, this will return false.
     protected boolean isFinished()
     {
-        return false;
+        return isTimedOut();
     }
 
     // Called once after isFinished returns true
--- src/net/pvschools/robotics/javabot/practice/commands/RunAutonomous.java
+++ src/net/pvschools/robotics/javabot/practice/commands/RunAutonomous.java
@@ -17,9 +17,18 @@ public class RunAutonomous extends CommandGroup
 {
     public RunAutonomous()
     {
+		// Add elements to smart dashboard for driving sideways:
+		SmartDashboard.putNumber("Sideways Spd", 0);
+		SmartDashboard.putNumber("Sideways Dir", 0);
+		SmartDashboard.putNumber("Sideways Sec", 0);
+		
 		// Note: 0 = left, 90 = backward, 180 = right, 270 = forward 
 		// TODO: Replace 0 speed used for testing with real speed
         addSequential(new DriveDirectional(0, 270), 1);
+		
+		// Intended for driving sideways:
+		addSequential(new DriveDirectional("Sideways Spd", "Sideways Dir", "Sideways Sec"));
+		
         addSequential(new WaitForGoal(), 6);
         addSequential(new Shoot());
     }    
